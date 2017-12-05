1180d229cc6f97ca52044e30f9f61f327b478515
MinnaVares <minna.vares@gmail.com>
2017-12-05 17:19:04 +0200

    Lisätty ajan muuttava metodi

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
-
+var time = undefined;
 class Sales extends Component {
+
+    changetime = () => {
+        var o = this.props.info.createDate;
+        var a = new Date(o);
+        var months = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var sec = a.getSeconds();
+        time = date + '.' + month + '.' + year + ' ' + hour + ':' + min + ':' + sec ;
+        return time;
+    }
     render () {
         return(
             <div>
-                <p>Ilmoitus jätetty: {this.props.info.createDate}</p>
+                <p>Ilmoitus jätetty: {this.changetime()}</p>
                 <p>Ilmoituksen sisältö: {this.props.info.content}</p>
-                <p>Käyttäjä: {this.props.info.seller.name}</p>
+                {/*<p>Käyttäjä: {this.props.info.seller.name}</p>*/}
                 <br/>
             </div>
         );
