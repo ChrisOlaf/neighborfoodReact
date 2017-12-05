2160613e454e9b96770edbedfc8ebc116d58ee21
MinnaVares <minna.vares@gmail.com>
2017-12-05 10:03:43 +0200

    Login komponentti, userin lisääminen stateen epävarmaa, mutta verify -metodi toimii

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,16 +3,44 @@ import React, { Component } from 'react';
 
 class Login extends Component{
 
-    state= {login: {email:'', password:''}};
+    state = {id:'',name:'', lastName:'', phoneNumber:'',
+        location:'',presentation:'',userStatus:'',
+        email:'',password:''};
+
+    verify = e => {
+        e.preventDefault();
+        var joku = this;
+        fetch('verify',
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify(this.state)
+            })
+            .then(function(res){
+                if(res.ok){
+                    return res.json();
+                console.dir("moimoi");}
+                else{
+                    throw new Error("something wrong with the response");
+                }
+            })
+
+     .catch(function(json){
+            console.dir("moi");
+            joku.setState(json);
+            return this.state;
+     });
+
+    };
 
     change = (e) => {
         this.setState({
-            [e.target.name]: e.target.value
-        });
+            [e.target.name]: e.target.value});
     };
-    verify = e => {
 
-    };
 
     render(){
         return(
@@ -36,6 +64,11 @@ class Login extends Component{
                                value="Kirjaudu sisään"
                                onClick={this.verify}/>
                     </form>
+                    <p>
+                       Nimi: {this.state.name}<br />
+                       maili: {this.state.email}<br />
+                       salasana {this.state.password}<br />
+                    </p>
                 </div>
         )
 
