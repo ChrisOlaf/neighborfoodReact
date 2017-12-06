ae2a8578499deb9009342661dfa2e6a38d7674a6
piapaula <paulamaki.pia@gmail.com>
2017-12-06 18:54:43 +0200

    Lisätty Responseform ja päivitetty orders.js

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import AddOrder from "./AddOrder";
+import Responseform from './Responseform';
+
 
 class Orders extends Component {
+
+    state = {
+        isFormVisible: false,
+    }
+
     render () {
         return(
             <div>
                 <p>Ilmoitus jätetty: {this.props.info.createDate}</p>
                 <p>Ilmoituksen tiedot: {this.props.info.content}</p>
-                {/*<p>Käyttäjä: {this.props.info.user.name}</p>*/}
+                <p>Käyttäjä: {this.props.info.user.name}</p>
+                <button onClick={() => this.setState({ isFormVisible: true })}>Vastaa tilaukseen</button>
                 <br/>
+                <div>
+                    { this.state.isFormVisible ? <Responseform orders_id={this.props.info.id} /> : null }
+                    { this.state.isFormVisible ? <button onClick={() => this.setState({ isFormVisible: false })}>Sulje</button> : null}
+                </div>
                 <AddOrder/>
             </div>
         );
