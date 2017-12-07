83a9769184cdc3d513c28fc50ed816372062d0c4
MinnaVares <minna.vares@gmail.com>
2017-12-07 09:59:42 +0200

    muokattu addOrder componenttia

--- src/components/AddOrder.js
+++ src/components/AddOrder.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 
 class AddOrder extends Component {
-    state = {orders: {content: '', requirements:[]}}
+    state = {orders: {content: '', user:{id:'1'}, requirements:[]}};
+
 
     handleInput = (e) => {
         this.setState({
-            [e.target.name]:e.target.value
+            [this.state.orders.content] : e.target.value
         });
     };
 
     addRequirement = (e) => {
+        this.state.orders.requirements.push(e.target.value);
+    };
 
+    addOrder = () => {
+        fetch('addorder')
     }
 
 
@@ -18,10 +23,17 @@ class AddOrder extends Component {
         return(
             <div>
             <form>
-                <input type="textarea" placeholder="Tilaus" name="content" value={this.state.value} onChange={this.handleInput}/>
-
+                <input type="textarea" placeholder="Tilaus" name="content" value={this.state.value} onChange={this.handleInput}/><br/>
+                <input type="checkbox" name="requirements" value="gluteeniton." onChange={this.addRequirement} />gluteeniton<br/>
+                <input type="checkbox" name="requirements" value="laktoositon." onChange={this.addRequirement}/>laktoositon<br/>
+                <input type="checkbox" name="requirements" value="maidoton." onChange={this.addRequirement} />maidoton<br/>
+                <input type="checkbox" name="requirements" value="viljaton." onChange={this.addRequirement}/>viljaton<br/>
+                <input type="checkbox" name="requirements" value="vegaani." onChange={this.addRequirement}/>vegaani<br/>
+                <input type="submit" onSubmit={this.addOrder} value="Lisää tilaus"/>
             </form>
-                <p>{this.state.orders.content}</p>
+                <p>Tähän pitäisi tulla tulokset:</p>
+                <p>sisältö: {this.state.orders.content}</p>
+                <p>reqs: {this.state.orders.requirements}</p>
             </div>
         );
     }
