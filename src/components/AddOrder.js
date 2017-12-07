1262293e487c5b3be2944163f5bc8a4326c591dc
MinnaVares <minna.vares@gmail.com>
2017-12-07 13:31:11 +0200

    muokattu addOrder componenttia

--- src/components/AddOrder.js
+++ src/components/AddOrder.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 
 class AddOrder extends Component {
-    state = {orders: {content: '', user:{id:'1'}, requirements:[]}};
+    state = {orders: {content: '', user:{id:''}, requirements:[]}};
 
+    cachedUser = window.sessionStorage.getItem('storedUser');
 
     handleInput = (e) => {
         this.setState({
@@ -14,9 +15,27 @@ class AddOrder extends Component {
         this.state.orders.requirements.push(e.target.value);
     };
 
-    addOrder = () => {
-        fetch('addorder')
+    addxtrReq = (e) =>{
+        this.state.orders.requirements.push(e.target.value);
     }
+
+    addOrder = (e) => {
+        e.preventDefault()
+        fetch('addorder',
+        {
+            headers: {
+                'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+            },
+            method: "POST",
+                body: JSON.stringify(this.state.orders)
+        })
+        .then(function(res){ console.log(res) })
+        .catch(function(res){ console.log(res) })
+
+        this.setState({orders: {content: '', user:{id:'1'}, requirements:[]}})
+    };
+
 
 
     render () {
@@ -24,11 +43,12 @@ class AddOrder extends Component {
             <div>
             <form>
                 <input type="textarea" placeholder="Tilaus" name="content" value={this.state.value} onChange={this.handleInput}/><br/>
-                <input type="checkbox" name="requirements" value="gluteeniton." onChange={this.addRequirement} />gluteeniton<br/>
-                <input type="checkbox" name="requirements" value="laktoositon." onChange={this.addRequirement}/>laktoositon<br/>
-                <input type="checkbox" name="requirements" value="maidoton." onChange={this.addRequirement} />maidoton<br/>
-                <input type="checkbox" name="requirements" value="viljaton." onChange={this.addRequirement}/>viljaton<br/>
-                <input type="checkbox" name="requirements" value="vegaani." onChange={this.addRequirement}/>vegaani<br/>
+                <input type="checkbox" name="requirements" value="gluteeniton" onChange={this.addRequirement} />gluteeniton<br/>
+                <input type="checkbox" name="requirements" value="laktoositon" onChange={this.addRequirement}/>laktoositon<br/>
+                <input type="checkbox" name="requirements" value="maidoton" onChange={this.addRequirement} />maidoton<br/>
+                <input type="checkbox" name="requirements" value="viljaton" onChange={this.addRequirement}/>viljaton<br/>
+                <input type="checkbox" name="requirements" value="vegaani" onChange={this.addRequirement}/>vegaani<br/>
+                Joku muu, mikä?<input type="text" name="requirements" value={this.state.value} onChange={this.addxtrReq}/><br/>
                 <input type="submit" onSubmit={this.addOrder} value="Lisää tilaus"/>
             </form>
                 <p>Tähän pitäisi tulla tulokset:</p>
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Redirect} from 'react-router-dom';
 import Orderlist from './Orderlist';
+import AddOrder from './AddOrder';
 import '../App.css';
 
 class Test extends React.Component {
@@ -9,6 +10,8 @@ class Test extends React.Component {
             <div className="Home">
                 <div className="ordersdiv">
                     <h1>Tilaukset</h1>
+                    <AddOrder/>
+                    <br/>
                     <Orderlist />
                 </div>
             </div>
