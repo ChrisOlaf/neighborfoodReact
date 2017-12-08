1709f607cec48a65679836004e158c35f8fd6f75
MinnaVares <minna.vares@gmail.com>
2017-12-08 12:40:16 +0200

    AddOrder ja AddSale

--- src/components/AddOrder.js
+++ src/components/AddOrder.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 var cachedUser;
+var orderAdded;
 class AddOrder extends Component {
-    state = {orders: {content: '', user:{}, requirements:[{requirement:''}]}};
+    state = {orders: {content: '', user: {}, requirements: [{requirement: ''}]}};
 
-    componentDidMount = () =>{
+    constructor(props) {
+        super(props);
+        orderAdded = false;
+    }
+
+
+    componentDidMount = () => {
         cachedUser = window.sessionStorage.getItem('storedUser');
         const x = this;
         if (cachedUser) {
             var user = JSON.parse(cachedUser);
-            x.setState({orders: {content: '',user: user, requirements:[{}]}});
+            x.setState({orders: {content: '', user: user, requirements: [{}]}});
 
             console.log();
             console.log(x.state);
@@ -22,13 +29,13 @@ class AddOrder extends Component {
 
     handleInput = (e) => {
         this.state.orders.content = e.target.value;
-        console.log("Handleinput: " +this.state);
-        console.log("Handleinput: " +this.state.orders.content);
+        console.log("Handleinput: " + this.state);
+        console.log("Handleinput: " + this.state.orders.content);
     };
 
     addRequirement = (e) => {
-        this.state.orders.requirements.push({requirement:e.target.value});
-        console.log("addreq: " +this.state);
+        this.state.orders.requirements.push({requirement: e.target.value});
+        console.log("addreq: " + this.state);
     };
 
     //
@@ -44,45 +51,80 @@ class AddOrder extends Component {
         console.log("Tätä yritetään lähettää" + this.state.orders);
         e.preventDefault();
         fetch('addorderwithreqs',
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify(this.state.orders)
-        })
-        .then(function(res){ console.log(res)
-            x.setState({orders: {content: '', user:{id:''}, requirements:[]}})})
-        .catch(function(res){ console.log(res) })
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify(this.state.orders)
+            })
+            .then(function (res) {
+                console.log(res)
+                x.setState({orders: {content: '', user: {id: ''}, requirements: []}})
+
+            })
+            .catch(function (res) {
+                console.log(res)
+            })
+        orderAdded = true;
+        this.render();
     };
 
+    // changeorderAdded = () =>{
+    //     console.log("Täällä ollaan");
+    //     console.log(orderAdded)
+    //     orderAdded = false;
+    //     console.log(orderAdded)
+    //     this.render()
+    // };
 
-
-    render () {
-        if(cachedUser){
-        return(
-            <div>
-                <form>
-                <input type="text" name="content" value={this.state.orders.content.value} onChange={e => this.handleInput(e)}/><br/>
-                <input type="checkbox" name="requirements" value= "gluteeniton" onChange={this.addRequirement} />gluteeniton<br/>
-                <input type="checkbox" name="requirements" value="laktoositon" onChange={this.addRequirement}/>laktoositon<br/>
-                <input type="checkbox" name="requirements" value="maidoton" onChange={this.addRequirement} />maidoton<br/>
-                <input type="checkbox" name="requirements" value="viljaton" onChange={this.addRequirement}/>viljaton<br/>
-                <input type="checkbox" name="requirements" value="vegaani" onChange={this.addRequirement}/>vegaani<br/>
-                {/*<input defaultValue="joku muu, mikä?" type="text" name="requirements" value={this.state.orders.requirements.requirement} onChange={e => this.addxtrReq(e)}/><br/>*/}
-                <input type="submit" onClick={e => this.addOrder(e)} value="Lisää tilaus"/>
-                </form>
-            </div>
-        );
-    }
-    else {
+    render() {
+        if (orderAdded) {
             return (
                 <div>
-                    <h1>Kirjaudu sisään lisätäksesi tilauksen</h1>
+                    <h2>Tilauksesi on nyt lisätty!</h2>
+                    {/*<button onClick={this.changeorderAdded}>Lisää uusi tilaus</button> <br />*/}
+                    {/*<button>Palaa etusivulle</button>*/}
                 </div>
             )
         }
+        else if (!orderAdded) {
+            if (cachedUser) {
+                return (
+                    <div>
+                        <h1>Lisää tilaus</h1>
+                        <h2>ohjeet:</h2>
+                        <ul>
+                            <li>Mitä ruokaa haluat syödä?</li>
+                            <li>milloin haluat syödä?</li>
+                            <li>noudatko itse vai kotiinkuljetus?</li>
+                            <li>hintahintahaarukka</li>
+                        </ul>
+                        <form>
+                            <textarea rows="4" cols="50" name="content" value={this.state.orders.content.value}
+                                      onChange={e => this.handleInput(e)}/><br/>
+                            <input type="checkbox" name="requirements" value="gluteeniton"
+                                   onChange={this.addRequirement}/>gluteeniton<br/>
+                            <input type="checkbox" name="requirements" value="laktoositon"
+                                   onChange={this.addRequirement}/>laktoositon<br/>
+                            <input type="checkbox" name="requirements" value="maidoton" onChange={this.addRequirement}/>maidoton<br/>
+                            <input type="checkbox" name="requirements" value="viljaton" onChange={this.addRequirement}/>viljaton<br/>
+                            <input type="checkbox" name="requirements" value="vegaani" onChange={this.addRequirement}/>vegaani<br/>
+                            {/*<input defaultValue="joku muu, mikä?" type="text" name="requirements" value={this.state.orders.requirements.requirement} onChange={e => this.addxtrReq(e)}/><br/>*/}
+                            <input type="submit" onClick={e => this.addOrder(e)} value="Lisää tilaus"/>
+                        </form>
+                    </div>
+                );
+            } else {
+                return (
+                    <div>
+                        <h1>Kirjaudu sisään lisätäksesi tilauksen</h1>
+                    </div>
+                )
+            }
+        }
     }
 }
+
 export default AddOrder;
--- src/components/AddSale.js
+++ src/components/AddSale.js
@@ -1,17 +1,103 @@
 import React, { Component } from 'react';
 import Orderlist from './Orderlist';
 import '../App.css';
-
+var cachedUser;
 class AddSale extends Component {
+    state = {sale: {content: '', user: {}, requirements: [{requirement: ''}]}};
+
+    componentDidMount = () => {
+        cachedUser = window.sessionStorage.getItem('storedUser');
+        const x = this;
+        if (cachedUser) {
+            var user = JSON.parse(cachedUser);
+            x.setState({sale: {content: '', user: user, requirements: [{}]}});
+
+            console.log();
+            console.log(x.state);
+            console.log("Haki käyttäjän storagesta!");
+        }
+        else {
+            console.log("Käyttäjää ei löytynyt storagesta..?");
+            this.addMessage();
+        }
+    };
+
+    handleInput = (e) => {
+        this.state.sale.content = e.target.value;
+        console.log("Handleinput: " + this.state);
+        console.log("Handleinput: " + this.state.sale.content);
+    };
+
+    addRequirement = (e) => {
+        this.state.sale.requirements.push({requirement: e.target.value});
+        console.log("addreq: " + this.state);
+    };
+
+    //
+    // addxtrReq = (e) =>{
+    //     this.state.orders.requirements.push({requirement: e.target.value});
+    // }
+
+    addMessage = () => {
+        console.log("Käyttäjää ei löytynyt");
+    }
+    addSale = (e) => {
+        var x = this;
+        console.log("Tätä yritetään lähettää" + this.state.sale);
+        e.preventDefault();
+        fetch('addsale',
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify(this.state.sale)
+            })
+            .then(function (res) {
+                console.log(res)
+                x.setState({sale: {content: '', user: {id: ''}, requirements: []}})
+            })
+            .catch(function (res) {
+                console.log(res)
+            })
+    };
+
+
     render() {
-        return (
-            <div className="Home">
-                <div className="ordersdiv">
-                    <h1>Lisää myynti</h1>
-                    <Orderlist />
+        if (cachedUser) {
+            return (
+                <div>
+                    <h1>Lisää myynti-ilmoitus</h1>
+                    <h2>ohjeet:</h2>
+                    <ul>
+                        <li>Mitä ruokaa valmistat?</li>
+                        <li>Mitä raaka-aineita käytät?</li>
+                        <li>Milloin ruoka haettavissa?</li>
+                        <li>hintapyyntö</li>
+                    </ul>
+
+                    <form>
+                        <textarea rows="5" cols="50" name="content" value={this.state.sale.content.value}
+                               onChange={e => this.handleInput(e)}/><br/>
+                        <input type="checkbox" name="requirements" value="gluteeniton" onChange={this.addRequirement}/>gluteeniton<br/>
+                        <input type="checkbox" name="requirements" value="laktoositon" onChange={this.addRequirement}/>laktoositon<br/>
+                        <input type="checkbox" name="requirements" value="maidoton" onChange={this.addRequirement}/>maidoton<br/>
+                        <input type="checkbox" name="requirements" value="viljaton" onChange={this.addRequirement}/>viljaton<br/>
+                        <input type="checkbox" name="requirements" value="vegaani" onChange={this.addRequirement}/>vegaani<br/>
+                        {/*<input defaultValue="joku muu, mikä?" type="text" name="requirements" value={this.state.sale.requirements.requirement} onChange={e => this.addxtrReq(e)}/><br/>*/}
+                        <input type="submit" onClick={e => this.addSale(e)} value="Lisää tilaus"/>
+                    </form>
                 </div>
-            </div>
-        );
+            );
+        }
+        else {
+            return (
+                <div>
+                    <h1>Kirjaudu sisään lisätäksesi myynnin</h1>
+                </div>
+            )
+        }
     }
 }
 
