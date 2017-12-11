87d358c43a1fcb36aa0d73df972d150d4b238984
piapaula <paulamaki.pia@gmail.com>
2017-12-11 12:11:45 +0200

    Sales.js / vastausten lisäys, haku ja tulostus lisätty.

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -10,22 +10,80 @@ class Sales extends Component {
             isFormVisible: false,
             responses: []
         }
+        this.handleClick = this.handleClick.bind(this);
     }
-    changetime = () => {
-        var o = this.props.info.createDate;
-        var a = new Date(o);
-        var months = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+
+    componentDidMount() {
+        this.goAndFetchData();
+    }
+
+    goAndFetchData = () => {
+        fetch('sale/'+this.props.info.id+'/responses')
+            .then(function (response) {
+                return response.json();
+            })
+            .then((function (jsonobject) {
+                this.setState({responses: jsonobject});
+            }).bind(this));
+
+    }
+
+    handleClick() {
+        this.setState({
+            isFormVisible: true
+        });
+    };
+
+    handleSendForm = (e) => {
+        e.preventDefault();
+        this.addResponse(e);
+    };
+
+    addResponse() {
+        const responseItem = {
+            content: this.state.content,
+            responder: this.props.user
+        }
+        fetch('sale/' + this.props.info.id + '/responses',
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify(responseItem)
+            }).then((function (response) {
+            this.goAndFetchData();
+        }).bind(this))
+
+
+        this.setState({isFormVisible: false})
+    };
+
+    changetime = (e) => {
+        var a = new Date(e);
+        var months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
         var year = a.getFullYear();
         var month = months[a.getMonth()];
         var date = a.getDate();
         var hour = a.getHours();
         var min = a.getMinutes();
         var sec = a.getSeconds();
-        time = date + '.' + month + '.' + year + ' ' + hour + ':' + min + ':' + sec ;
+        time = date + '.' + month + '.' + year + ' ' + hour + ':' + min + ':' + sec;
         return time;
     };
-    render () {
+
+    render() {
         let form = null;
+        var responses = this.state.responses.map(function (response) {
+            return (
+                <div key={response.id}>
+                    <p>Vastauksen sisältö: {response.content}</p>
+                    <p>Lähettäjä: {response.responder.name}</p>
+                    <p>Vastaus jätetty: {response.createDate} </p>
+                </div>
+            )
+        })
         if (this.state.isFormVisible) {
             form =
                 <form>
@@ -47,12 +105,14 @@ class Sales extends Component {
         if (this.props.auth === true) {
             responseButton = <button onClick={this.handleClick}>Vastaa tilaukseen</button>
         }
-        return(
+        return (
             <div>
-                <p>Ilmoitus jätetty: {this.changetime()}</p>
+                <p>Ilmoitus jätetty: {this.changetime(this.props.info.createDate)}</p>
+                <p>Ilmoituksen otsikko: {this.props.info.title}</p>
                 <p>Ilmoituksen sisältö: {this.props.info.content}</p>
-                {/*<p>Käyttäjä: {this.props.info.seller.name}</p>*/}
+                <p>Käyttäjä: {this.props.info.user.name}</p>
                 <h4>Vastaukset</h4>
+                {responses}
                 {responseButton}
                 <div>
                     {form}
