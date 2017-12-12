39502a5b47c08559d94ae3abf1b6bdb90a29535d
MinnaVares <minna.vares@gmail.com>
2017-12-12 17:37:03 +0200

    Keskeneräinen arvostelujenlisäysformi ja käyttäjänhaku-profiilisivu

--- src/components/User.js
+++ src/components/User.js
@@ -1,27 +1,62 @@
 import React, {Component} from 'react';
-
+var cachedUser;
 
 class User extends Component {
-    state = {id:this.props.id, auth: this.props.auth};
+    state = {user: '', data: '', review:{content:'', stars: ''}};
+
+    getUser() {
+        fetch(this.props.match.params.id)
+            .then(function (response) {
+                return response.json();
+            })
+            .then((function (jsonobject) {
+                this.setState({data: jsonobject});
+            }).bind(this));
+    }
+
+    componentDidMount = () => {
+        cachedUser = window.sessionStorage.getItem('storedUser');
+        const x = this;
+        if (cachedUser) {
+            var user = JSON.parse(cachedUser);
+            x.setState({user:user});
+            console.log("Haki käyttäjän storagesta!");
+        }
+        else {
+            console.log("Käyttäjää ei löytynyt storagesta..?");
+        }
+        this.getUser();
+    };
+
+    addReview = (e) => {
+        e.preventDefault()
+    };
     render() {
-
-        const id = this.props.location.state;
-        console.log(id);
-        console.log("näytä state:.")
-        console.dir(
-            this.props.state
-        );
-
-        console.dir(
-            this.props.id
-        )
+        // var reviews = this.state.data.reviews.map(function (review) {
+        //      return (
+        //             <div key={review.id}>
+        //                 {review.content}
+        //             </div>
+        //         )
+        // })
         return (
-            <span>
-                <h1>Tässä pitäisi näkyä state:</h1>
-                <h1>{this.state.id}</h1>
-                <h1>{this.state.auth}</h1>
-                <h1>{this.props.match.params.id}</h1>
-            </span>
+            <div>
+                <h1>Kirjautuneen käyttäjän nimi:{this.state.user.name}</h1>
+                <h1> haettu käyttäjä: {this.state.data.name}</h1>
+                <h1>ID: {this.state.data.id}</h1>
+                <div>
+                    <p>Reviews</p>
+                    <p>Tähän tulee saadut arvostelut</p>
+                </div>
+                <div>
+                    <h1>Lisää arvostelu</h1>
+                    <form>Arvostelu:
+                        <input type="text" name="content" value={this.state.review.content.value}/><br/>
+                        Tähtiä 1-5: <input type="number" min="1" max="5" name="stars" value={this.state.review.stars.value}/>
+                        <input type="submit" onClick={this.addReview}/>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
