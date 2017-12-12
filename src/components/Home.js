b2d9c11186b51514e2929dd5e89726ab543b7619
Risto Rautanen <rautanen.r@gmail.com>
2017-12-12 16:48:58 +0200

    Alustavaa muotoilua etusivulle.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,50 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Row, Col} from 'react-bootstrap';
+
 import '../App.css';
 import Orderlist from './Orderlist';
 import Saleslist from "./Saleslist";
 
 class Home extends Component {
     render() {
-        return (
-            <div className="Home">
-                <div className="ordersDiv">
-                    <h1>Tilaukset</h1>
-                    <Orderlist auth={this.props.auth} user={this.props.user}/>
+        if (this.props.user.userStatus === "chef") {
+            return (
+                <div className="container-fluid">
+                    <Row className="show-grid">
+                        <Col xs={12} md={4} mdOffset={1}>
+                            <div className="content-div">
+                                <h1>Tilaukset</h1>
+                                <Orderlist auth={this.props.auth} user={this.props.user}/>
+                            </div>
+                        </Col>
+                        <Col xs={12} md={4} mdOffset={2}>
+                            <div className="content-div">
+                                <h1>Myynnit</h1>
+                                <Saleslist auth={this.props.auth} user={this.props.user}/>
+                            </div>
+                        </Col>
+                    </Row>
                 </div>
-                <div className="salesDiv">
-                    <h1>Myynnit</h1>
-                    <Saleslist auth={this.props.auth} user={this.props.user}/>
-                </div>
-            </div>
-        );
+            );
+        }
+        else {
+            return (
+                    <Row className="show-grid">
+                        <Col xs={12} sm={6} md={5} mdOffset={1}>
+                            <div className="content-div">
+                                <h1>Myynnit</h1>
+                                <Saleslist auth={this.props.auth} user={this.props.user}/>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={6} md={5}>
+                            <div className="content-div">
+                                <h1>Tilaukset</h1>
+                                <Orderlist auth={this.props.auth} user={this.props.user}/>
+                            </div>
+                        </Col>
+                    </Row>
+            );
+        }
     }
 }
 
