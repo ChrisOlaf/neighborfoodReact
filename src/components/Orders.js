133d0ddf844e8957ff5e0ca2bd5786ab947b0984
MinnaVares <minna.vares@gmail.com>
2017-12-13 16:14:07 +0200

    ajanmuutosmetodin kunnostus

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -91,9 +91,9 @@ class Orders extends Component {
         var year = a.getFullYear();
         var month = months[a.getMonth()];
         var date = a.getDate();
-        var hour = a.getHours();
-        var min = a.getMinutes();
-        var sec = a.getSeconds();
+        var hour = a.getHours()< 10 ? '0'+a.getHours():a.getHours();
+        var min = a.getMinutes()< 10 ? '0'+a.getMinutes():a.getMinutes();
+        var sec = a.getSeconds()< 10 ? '0'+a.getSeconds(): a.getSeconds();
         time = date + '.' + month + '.' + year + ' ' + hour + ':' + min + ':' + sec;
         return time;
     };
